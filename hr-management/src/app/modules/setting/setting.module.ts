755f40f100ffa4569a59567b7b214cb35a45ccb8
DamThiLanAnh <damthilananh8@hmail.com>
2025-12-24 14:57:09 +0700

    update

---
+++ hr-management/src/app/modules/setting/setting.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { SettingComponent } from './setting.component';
+
+@NgModule({
+  imports: [],
+  declarations: [
+    SettingComponent
+  ],
+  exports: []
+})
+export class SettingModule { }
