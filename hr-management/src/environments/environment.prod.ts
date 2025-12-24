243dd064d52840b348453a78db11bab134fae959
DamThiLanAnh <damthilananh8@hmail.com>
2025-12-24 13:42:34 +0700

    update

--- hr-management/src/environments/environment.prod.ts
+++ hr-management/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hrm-backend-1939.onrender.com'
+  apiUrl: 'https://hrm-backend-1939.onrender.com',
+  bearToken: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTc2NTI5MDkzOSwiZXhwIjoxNzY1ODk1NzM5fQ.tbcwbrOjIAv-WzOUovC6kWaiAO0-xkxpwsY3pY9imNE'
 };
 
