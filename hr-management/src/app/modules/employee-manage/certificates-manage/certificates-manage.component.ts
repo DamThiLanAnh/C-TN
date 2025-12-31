b214824be917ee4f05ca78835f64cd919a1178b0
DamThiLanAnh <damthilananh8@hmail.com>
2025-12-31 14:32:06 +0700

    component certificates

---
+++ hr-management/src/app/modules/employee-manage/certificates-manage/certificates-manage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-certificates-manage',
+  templateUrl: './certificates-manage.component.html',
+  styleUrls: ['./certificates-manage.component.scss']
+})
+export class CertificatesManageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
