7e4c341028c09ba69d40470f0d2869b657bee4b0
DamThiLanAnh <damthilananh8@hmail.com>
2026-01-05 09:00:12 +0700

    update fix bug

---
+++ hr-management/src/app/modules/import-data/import-salary/import-salary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-import-salary',
+  templateUrl: './import-salary.component.html',
+  styleUrls: ['./import-salary.component.scss']
+})
+export class ImportSalaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
